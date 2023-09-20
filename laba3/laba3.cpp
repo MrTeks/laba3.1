577cb8173e0e41cec49d0594d436bcc9ee22557c
Teks <Teks@TEKS>
2023-09-20 15:43:44 +0700

    Добавьте файлы проекта.

---
+++ laba3/laba3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int a, b, c;
+	c = 0;
+	cout << "Введите первое число:" << endl;
+	cin >> a;
+	cout << "Введите второе число" << endl;
+	cin >> b;
+	if (a % 2 == 1) {
+		c += 1;
+	}
+	if (b % 2 == 1) {
+		c += 1;
+	}
+	if (c == 1) {
+		cout << "Истина" << endl;
+	}
+	else if (c != 1) {
+		cout << "Ложь" << endl;
+	}
+}
